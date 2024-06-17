ff1ee8aeca9f71e96dc3ff344df2feaeea1aa212
Priyanjith N <jithpriyan2006@gmail.com>
2024-06-17 18:53:51 +0530

    home naviagtion option set for login page

--- src/app/features/auth/user/login-page/login-page.component.ts
+++ src/app/features/auth/user/login-page/login-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../../../../shared/components/login-form/login-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [
+    LoginFormComponent,
+    RouterLink
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
