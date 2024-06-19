f4f363c4a24cacd296f7a3aa1123891f769cb585
Priyanjith N <jithpriyan2006@gmail.com>
2024-06-19 12:47:50 +0530

    register page added with form

--- src/app/shared/components/register-form/register-form.component.ts
+++ src/app/shared/components/register-form/register-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  toggleShowHidePassword: boolean = false;
+  toggleShowHideConfirmPassword: boolean = false;
 
+  toggleShowHide(password: boolean = true): void {
+    if(password) {
+      this.toggleShowHidePassword = !this.toggleShowHidePassword;
+    }else{
+      this.toggleShowHideConfirmPassword = !this.toggleShowHideConfirmPassword;
+    }
+  }
 }
