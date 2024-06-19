fdb62c53eb1081bb089a8ccc02152e10b186191a
Priyanjith N <jithpriyan2006@gmail.com>
2024-06-19 11:59:40 +0530

    otp-finshed with service

--- src/app/features/auth/user/register-page/register-page.component.ts
+++ src/app/features/auth/user/register-page/register-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RegisterFormComponent } from '../../../../shared/components/register-form/register-form.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RegisterFormComponent
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
