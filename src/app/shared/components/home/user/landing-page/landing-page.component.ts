9f82e8b30d453c5ea2531f732146535fe7b4287e
Priyanjith N <jithpriyan2006@gmail.com>
2024-07-19 18:51:49 +0530

    card slider done user side

--- src/app/shared/components/home/user/landing-page/landing-page.component.ts
+++ src/app/shared/components/home/user/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CardSliderComponent } from '../card-slider/card-slider.component';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    CardSliderComponent
+    CardSliderComponent,
+    CarouselComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
