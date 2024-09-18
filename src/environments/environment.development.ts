1a48bdd855b4fd395f0f85f1e8b575335929012c
Priyanjith N <jithpriyan2006@gmail.com>
2024-09-18 17:20:49 +0530

    env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    BACKEND_DOMAIN: 'http://localhost:8080',
+    GOOGLE_PROVIDER_CLIENT_ID: '414347837991-h5cf9inco1klhf57sb9j9sbp6rvrbavb.apps.googleusercontent.com',
+    GEOAPIFY_API_KEY: 'b4e12229e72e42c2b0b9cb2932e3211d',
+    GEOAPIFY_AUTOCOMPLETE_API: 'https://api.geoapify.com/v1/geocode/autocomplete',
+    GEOAPIFY_REVERSE_GEOCODING_API: 'https://api.geoapify.com/v1/geocode/reverse',
+    stripe: {
+        publicKey: 'pk_test_51PwiKlEJvzUku5XglgRB9JhdO3LSSrGc5F1MQEsWyzhauuHaJbkcwNBk7wVOK84dlIcKOQihfN3emZVkxytvb5it0028pFcNsQ',
+    }
+};
