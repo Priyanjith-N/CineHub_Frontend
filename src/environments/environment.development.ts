e59f3abd488449e1e0fc0ba9803afd6c3116f5cc
Priyanjith N <jithpriyan2006@gmail.com>
2024-09-25 12:08:49 +0530

    dash for theater owner side partialy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    BACKEND_DOMAIN: 'http://localhost:8080' || 'https://cinehub-backend.priyanjith.online',
+    BACKEND_DOMAIN: 'https://cinehub-backend.priyanjith.online',
     GOOGLE_PROVIDER_CLIENT_ID: '414347837991-h5cf9inco1klhf57sb9j9sbp6rvrbavb.apps.googleusercontent.com',
     GEOAPIFY_API_KEY: 'b4e12229e72e42c2b0b9cb2932e3211d',
     GEOAPIFY_AUTOCOMPLETE_API: 'https://api.geoapify.com/v1/geocode/autocomplete',
